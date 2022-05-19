fdadcde497b99b6fca60b28e79d7715141feac75
sandip suryawanshi <suryawanshisandip299@gmail.com>
2022-05-19 16:31:43 +0530

    react 1st

---
+++ src/Components/Manufacture.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const Manufacture = () => {
+  return (
+    <div>
+      <h1>Mobile Manufactureres</h1> 
+      <li>Samsung</li>
+      <li>HTC</li>
+      <li>Micromax</li>
+      <li>Apple</li>
+      
+    </div>
+  )
+}
+
+export default Manufacture
---
+++ src/Components/operating.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+let Operating = () => {
+  return (
+    <div >
+      <h1>Mobile Operating Systems</h1>
+
+      <li>Android</li>
+      <li>Blackberry</li>
+      <li>iPhone</li>
+      <li>Windows Phone</li>
+    </div>
+  );
+};
+
+export default Operating;
